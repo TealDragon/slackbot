8ac8b4cd475bc6019ebf41c6fe2b40ae8f21032d
Casey Shew <cshew@ecornell.com>
2021-05-11 17:32:31 -0400

    Added placeholder file for search logic, to be implemented into app.py

--- csgbot.py
+++ csgbot.py
@@ -12,9 +12,9 @@ env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
 app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
+slack_event_adapter = SlackEventAdapter(os.environ['SLACK_SIGNING_SECRET'],'/slack/events',app)
 
-client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
+client = slack.WebClient(token=os.environ['SLACK_BOT_TOKEN'])
 BOT_ID = client.api_call("auth.test")['user_id']
 
 @slack_event_adapter.on('message')
